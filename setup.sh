27b5b8fa8d7cf8def234133900b9f15243faec86
agwermann <alexandrewermann@gmail.com>
2023-04-07 23:55:44 -0300

    add setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 
 # Create cluster
-kind create cluster
+kind create cluster --config scripts/kind-config.yaml
 
 # Load docker images
 kind load docker-image agwermann/manufactoring-service:0.1
@@ -10,14 +10,25 @@ kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1
 kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.8.0/serving-core.yaml
 kubectl get pods --namespace knative-serving
 
+# Install Istio
+kubectl apply -l knative.dev/crd-install=true -f https://github.com/knative/net-istio/releases/download/knative-v1.8.0/istio.yaml
+kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.8.0/istio.yaml
+kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.8.0/net-istio.yaml
+kubectl --namespace istio-system get service istio-ingressgateway
+kubectl get pods --namespace knative-serving
+kubectl get pods --namespace istio-system
+
 # Install Eventing dependencies
 kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.8.0/eventing-crds.yaml
 kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.8.0/eventing-core.yaml
 kubectl get pods --namespace knative-eventing
 
 # Create namespace
-kubectl apply -f config/samples/0-dt-core.yaml
+kubectl apply -f config/samples/0-dt-namespaces.yaml
 
 # Install Camel-K
-kubectl -n default create secret docker-registry external-registry-secret --docker-username agwermann --docker-password <PASSWORD> -n dt-core
-kamel install --operator-image=docker.io/apache/camel-k:1.10.3 --olm=false -n dt-core --global --registry docker.io --organization agwermann --registry-secret external-registry-secret --force
+kubectl create secret docker-registry external-registry-secret --docker-username agwermann --docker-password <DOCKER_PASSWORD> -n default
+kamel install --operator-image=docker.io/apache/camel-k:1.10.3 --olm=false -n default --global --registry docker.io --organization agwermann --registry-secret external-registry-secret --force
+
+# HTTP Bin
+kubectl apply -f https://github.com/istio/istio/raw/master/samples/httpbin/httpbin.yaml
